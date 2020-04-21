7ca3fe97119cfba4ea343c7112d213d5113e5d96
Leon Calermo <lcalermo@MacBook-Pro-de-wolox-109.local>
2020-04-21 11:41:10 -0300

    removed unused method in appliation controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,6 @@ class ApplicationController < ActionController::API
     I18n.with_locale(locale, &action)
   end
 
-  def render_error(status, message)
-    render json: message, status: status
-  end
-
   rescue_from ActiveRecord::RecordNotFound do |error|
     render json: { errors: error }, status: :bad_request
   end
